f014dd66f703648c251d3230532b591a6634d30c
Jan Machala <jan.machala@email.cz>
2012-10-26 15:19:09 +0200

    ParentConstructorCallSniff does no exceptions

--- tests/Collabim/Sniffs/Classes/ParentConstructorCallSniffTest.php
+++ tests/Collabim/Sniffs/Classes/ParentConstructorCallSniffTest.php
@@ -9,33 +9,21 @@ class Collabim_Sniffs_Classes_ParentConstructorCallSniffTest extends Collabim_Te
 		$result = $this->checkFile(__DIR__ . '/data/ParentConstructorCallSniff.php');
 		$this->assertEquals(3, $result['numErrors']);
 
-		$message = 'Missing parent constructor call. If you don\'t call the parent constructor on purpose, suppress this warning by using @SuppressWarnings("CS.ParentConstructorCall") annotation. '
-			. 'If the parent class does not have a constructor, add this class to the list of ignored classes in this sniff.';
+		$message = 'Missing parent constructor call. If you don\'t call the parent constructor on purpose, suppress this warning by using @SuppressWarnings("CS.ParentConstructorCall") annotation.';
 
 		$this->assertEquals(
 			$message,
-			$result['errors'][86][9][0]['message']
+			$result['errors'][21][9][0]['message']
 		);
 
 		$this->assertEquals(
 			$message,
-			$result['errors'][96][9][0]['message']
+			$result['errors'][31][9][0]['message']
 		);
 
 		$this->assertEquals(
 			$message,
-			$result['errors'][106][9][0]['message']
-		);
-	}
-
-	public function testCheckIgnoredClass()
-	{
-		$result = $this->checkFile(__DIR__ . '/data/ignored-class.php');
-		$this->assertEquals(1, $result['numErrors']);
-
-		$this->assertEquals(
-			'Collabim\TestClass is in ignored classes but contains constructor. Please update this sniff accordingly.',
-			$result['errors'][24][1][0]['message']
+			$result['errors'][41][9][0]['message']
 		);
 	}
 
